55e1406b82ff920b71c413490b0063276d5d3af2
AdeolaA-ui <82666268+AdeolaA-ui@users.noreply.github.com>
2021-04-20 13:58:32 -0600

    -- page bottom and page top deleted

--- custom/pageBottom.js
+++
@@ -1,8 +0,0 @@
-document.write('<div id="bottomBar">');
-// document.write('<hr>');
-// Centered:
-// Logo, just the logo.
-// document.write('<img src="custom/logo.jpg" alt="logo"/>');
-document.write('</div>');
-document.write('<div style="padding-top: 10px;">2019 Tiger Farm Press</div>');
-document.write('</div>');
--- custom/pageTop.js
+++
@@ -1,4 +0,0 @@
-document.write('<div id="topBar">');
-document.write('<table><tr>');
-document.write('</tr></table>');
-document.write('</div>');
